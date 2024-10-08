75082e076a696a72ddda9b70aea63a91a9678169
kelvinempire <kelvinewurum@gmail.com>
2024-10-08 02:37:32 +0100

    DOM3

--- script.js
+++ script.js
@@ -17,12 +17,12 @@ redColor.addEventListener("click", function () {
 blackColor.addEventListener("click", function () {
   cartBtn.style.backgroundColor = "black";
   itemTag.style.backgroundColor = "black";
+  itemTag.style.color = "white";
   imageCard.src =
     "https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?cs=srgb&dl=pexels-mikebirdy-112460.jpg&fm=jpg;";
 });
 grayColor.addEventListener("click", function () {
   cartBtn.style.backgroundColor = "gray";
   itemTag.style.backgroundColor = "gray";
-  imageCard.src =
-    "https://cdn.pixabay.com/photo/2012/05/29/00/43/car-49278_640.jpg";
+  imageCard.src = "./image/img.png";
 });
